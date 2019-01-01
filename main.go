0a2bb3b2a475b7bd9067980356129a0a91e1419a
waldemar <waldemar@wdmsys.com>
2018-12-31 18:55:49 -0800

    select only interesting types

--- main.go
+++ main.go
@@ -1,3 +1,5 @@
+/* Copyright (c) 2018-2019 Waldemar Augustyn */
+
 package main
 
 import (
@@ -9,9 +11,7 @@ func main() {
 
 	t := new(dns.Transfer)
 	m := new(dns.Msg)
-	//t.TsigSecret = map[string]string{"axfr.": "so6ZGir4GPAqINNh9U5c3A=="}
 	m.SetAxfr("wdmsys.com.")
-	//m.SetTsig("axfr.", dns.HmacMD5, 300, time.Now().Unix())
 	c, err := t.In(m, "192.168.84.7:53")
 	if err != nil {
 		fmt.Printf("transfer failed: %v\n", err)
@@ -21,7 +21,11 @@ func main() {
 			fmt.Printf("envelope error: %v\n", e.Error)
 		} else {
 			for _, r := range e.RR {
-				fmt.Printf("%v\n", r)
+				// only CNAME and SOA
+				rrtype := r.Header().Rrtype
+				if rrtype == dns.TypeCNAME || rrtype == dns.TypeSOA {
+					fmt.Printf("%v\n", r)
+				}
 			}
 		}
 	}
