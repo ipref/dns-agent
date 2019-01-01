7c8bc8db8216d6d8ce7616f24fa2432d23c1d4a0
waldemar <waldemar@wdmsys.com>
2019-01-01 13:31:57 -0800

    ignore envelope errors

--- main.go
+++ main.go
@@ -11,22 +11,22 @@ func main() {
 
 	t := new(dns.Transfer)
 	m := new(dns.Msg)
-	m.SetAxfr("wdmsys.com.")
-	c, err := t.In(m, "192.168.84.7:53")
+	m.SetAxfr("ipref.org.")
+	c, err := t.In(m, "ns1.dynu.com:53")
 	if err != nil {
 		fmt.Printf("transfer failed: %v\n", err)
 	}
 	for e := range c {
 		if e.Error != nil {
 			fmt.Printf("envelope error: %v\n", e.Error)
-		} else {
-			for _, r := range e.RR {
-				// only CNAME and SOA
-				rrtype := r.Header().Rrtype
-				if rrtype == dns.TypeCNAME || rrtype == dns.TypeSOA {
-					fmt.Printf("%v\n", r)
-				}
+		}
+		for _, r := range e.RR {
+			// only selected records
+			rrtype := r.Header().Rrtype
+			if rrtype == dns.TypeA || rrtype == dns.TypeTXT || rrtype == dns.TypeSOA {
+				fmt.Printf("%v\n", r)
 			}
 		}
+
 	}
 }
