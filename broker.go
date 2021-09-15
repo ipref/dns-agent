20e9bfbed41ac5d48b250bbce72fc73aedf6e51c
waldemar <waldemar@wdmsys.com>
2021-09-15 06:53:48 -0700

    aggregate server data

--- broker.go
+++ broker.go
@@ -1,4 +1,4 @@
-/* Copyright (c) 2018-2019 Waldemar Augustyn */
+/* Copyright (c) 2018-2021 Waldemar Augustyn */
 
 package main
 
@@ -12,12 +12,18 @@ import (
 /* Broker operations
 
 The broker consolidates data from different DNS servers to a single set per
-each combination of local domain and ipref domain. It counts the number of
-consecutive data signaled from servers. If the count reaches designated value,
-typically 2, it marks the data as current. This is the data that is advertized
-to the mapper.
-
+each combination of local domain and ipref domain. It requires quorum among
+servers to declare data valid before sending to the mapper.
 */
+
+const (
+
+	// state codes
+
+	NEW = iota
+	SENT
+	ACKED
+)
 
 type M32 int32 // mark, stamp/counter provided by the mapper
 type O32 int32 // id associated with source, provided by the mapper
@@ -30,21 +36,28 @@ func (ip IP32) String() string {
 	return net.IP(addr).String()
 }
 
-type Host struct {
-	ip   IP32
-	name string
-}
-
 type IprefAddr struct {
 	gw  IP32
 	ref ref.Ref
 }
 
-//type MapStatus struct {
-//	current *MapData
-//	last    *MapData
-//	count   int
-//}
+type Host struct {
+	ip   IP32
+	name string
+}
+
+type Status struct {
+	Host
+	state  int
+	batch  int  // batch id to match acks
+	remove bool // remove item from DNS records
+}
+
+type MapData struct {
+	source string
+	hash   uint64               // current hash
+	hstat  map[IprefAddr]Status // host status
+}
 
 type StateData struct {
 	source string
@@ -53,17 +66,19 @@ type StateData struct {
 }
 
 type SrvData struct {
-	hash  uint64
-	hosts map[IprefAddr]Host
+	server string
+	hash   uint64
+	hosts  map[IprefAddr]Host
 }
 
-type AggData struct {
+type AggData struct { // data from all servers for a source
+	source    string
 	quorum    int
 	hash_sent uint64
 	srvdata   map[string]SrvData
 }
 
-type DnsData struct {
+type DnsData struct { // data from a single server
 	source string
 	server string
 	hash   uint64
@@ -74,11 +89,44 @@ var be = binary.BigEndian
 
 var sources map[string][]string // source -> [server1:port, server2:port, ...]
 var aggdata map[string]AggData  // source -> aggdata -> srvdata
+var mapdata map[string]MapData  // source -> host status
 
 //var mstat map[string]*MapStatus
 var dnsdataq chan DnsData
 var statedataq chan StateData
 var mreqq chan (*MreqData)
+
+func new_state(sd StateData) {
+
+	md, ok := mapdata[sd.source]
+	if !ok {
+		md.source = sd.source
+		md.hstat = make(map[IprefAddr]Status)
+		mapdata[sd.source] = md
+	}
+
+	if sd.hash == md.hash {
+		return // nothing new
+	}
+
+	// update map
+
+	for _, hs := range md.hstat {
+		hs.remove = true
+	}
+
+	for iraddr, host := range sd.hosts {
+		hs, ok := md.hstat[iraddr]
+		if !ok || hs.ip != host.ip || hs.name != host.name {
+			hs.ip = host.ip
+			hs.name = host.name
+			hs.state = NEW
+		}
+		hs.remove = false
+	}
+
+	md.hash = sd.hash
+}
 
 func new_data(data DnsData) {
 
@@ -86,39 +134,40 @@ func new_data(data DnsData) {
 
 	agg, ok := aggdata[data.source]
 	if !ok {
+		agg.source = data.source
 		agg.quorum = len(sources[data.source])/2 + 1
 		agg.srvdata = make(map[string]SrvData)
 	}
 
-	sdata := agg.srvdata[data.server]
+	srv, ok := agg.srvdata[data.server]
+	if !ok {
+		srv.server = data.server
+	}
 
-	sdata.hash = data.hash
-	sdata.hosts = data.hosts
+	srv.hash = data.hash
+	srv.hosts = data.hosts
 
-	agg.srvdata[data.server] = sdata
+	agg.srvdata[data.server] = srv
 
 	// check if we have a quorum (number of servers with the same hash)
 
 	qcount := make(map[uint64]int)
 
-	for _, sdata := range agg.srvdata {
+	for _, srv := range agg.srvdata {
 
-		count := qcount[sdata.hash]
+		count := qcount[srv.hash]
 		count++
-		qcount[sdata.hash] = count
+		qcount[srv.hash] = count
 
 		if count == agg.quorum {
-			if sdata.hash != agg.hash_sent {
+			if srv.hash != agg.hash_sent {
 				// new data reached quorum
-				agg.hash_sent = sdata.hash
-				statedataq <- StateData{data.source, sdata.hash, sdata.hosts}
+				agg.hash_sent = srv.hash
+				statedataq <- StateData{data.source, srv.hash, srv.hosts}
 			}
 			break
 		}
 	}
-}
-
-func new_state(state StateData) {
 }
 
 func new_mapper_request(mreq *MreqData) {
