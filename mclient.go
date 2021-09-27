717e7109b282161498c02a49ef05990c96c5edf7
waldemar <waldemar@wdmsys.com>
2021-09-26 23:47:20 -0700

    update packet format to current V1 protocol

--- mclient.go
+++ mclient.go
@@ -8,28 +8,56 @@ import (
 	"log"
 	"math/rand"
 	"net"
+	"strings"
 	"time"
 )
 
 const (
 	RECONNECT = 17   // [s] delay between reconnect
 	MAXPKTLEN = 1200 // max size of packet payload
-	// mapper request codes
-	GET_CURRENT  = 1
-	SEND_CURRENT = 2
-	GET_RECORDS  = 3
-	SEND_RECORDS = 4
-	// pkt constants
-	V1_SIG         = 0x11 // v1 signature
-	V1_HDR_LEN     = 8
-	V1_TYPE_AREC   = 1
+)
+
+const ( // v1 constants
+
+	V1_SIG      = 0x11 // v1 signature
+	V1_HDR_LEN  = 8
+	V1_AREC_LEN = 4 + 4 + 4 + 8 + 8 // ea + ip + gw + ref.h + ref.l
+	// v1 header offsets
+	V1_VER      = 0
+	V1_CMD      = 1
+	V1_PKTID    = 2
+	V1_RESERVED = 4
+	V1_PKTLEN   = 6
+	// v1 arec offsets
+	V1_AREC_EA   = 0
+	V1_AREC_IP   = 4
+	V1_AREC_GW   = 8
+	V1_AREC_REFH = 12
+	V1_AREC_REFL = 20
+)
+
+const ( // v1 item types
+
+	//V1_TYPE_NONE   = 0
+	//V1_TYPE_AREC   = 1
+	//V1_TYPE_SOFT   = 2
+	//V1_TYPE_IPV4   = 3
 	V1_TYPE_STRING = 4
-	V1_AREC_LEN    = 4 + 4 + 4 + 8 + 8 // ea + ip + gw + rel.h + ref.l
-	// pkt types
-	V1_GET_SOURCE_INFO    = 6
-	V1_SOURCE_INFO        = 7
-	V1_GET_SOURCE_RECORDS = 8
-	V1_SOURCE_RECORDS     = 9
+)
+
+const ( // v1 commands
+
+	V1_NOOP             = 0
+	V1_MC_HOST_DATA     = 14
+	V1_MC_GET_HOST_DATA = 15
+)
+
+const ( // v1 command mode, top two bits
+
+	V1_DATA = 0x00
+	V1_REQ  = 0x40
+	V1_ACK  = 0x80
+	V1_NACK = 0xC0
 )
 
 type AddrRec struct {
@@ -48,124 +76,71 @@ var sendreqq chan SendReq
 var pktidq chan uint16
 
 func send_to_mapper(conn *net.UnixConn, connerr chan<- string, req SendReq) {
-	/*
-		var pkt [MAXPKTLEN]byte
-		var wlen int
-		var off int
 
-		switch req.cmd {
-		case SEND_CURRENT:
+	var pkt [MAXPKTLEN]byte
+	var off int
 
-			if minlen := 8 + 4 + 8 + len(req.data.(MreqSendCurrent).source) + 6; len(pkt) < minlen {
-				log.Printf("E mclient write: packet buffer too short %v, needs %v",
-					len(pkt), minlen)
-				return
+	// V1 header
+
+	pkt[V1_VER] = V1_SIG
+	pkt[V1_CMD] = V1_REQ | V1_MC_HOST_DATA
+	be.PutUint16(pkt[V1_PKTID:V1_PKTID+2], <-pktidq)
+	pkt[V1_RESERVED] = 0
+	pkt[V1_RESERVED+1] = 0
+
+	off = V1_HDR_LEN
+
+	// batch id
+
+	be.PutUint32(pkt[off:off+4], req.batch)
+
+	off += 4
+
+	// source
+
+	for _, src := range strings.Split(req.source, ":") {
+
+		dnm := []byte(src)
+		dnmlen := len(dnm)
+
+		if 0 < dnmlen && dnmlen < 256 { // should be true since DNS names are shorter than 255 chars
+
+			pkt[off] = V1_TYPE_STRING
+			pkt[off+1] = byte(dnmlen)
+			copy(pkt[off+2:], dnm)
+
+			for off += dnmlen + 2; off < (dnmlen+5)&^3; off++ {
+				pkt[off] = 0
 			}
 
-			source_len := len(req.data.(MreqSendCurrent).source)
-
-			if source_len > 255 {
-				log.Printf("E mclient write: source name too long: %v",
-					req.data.(MreqSendCurrent).source)
-				return
-			}
-
-			// header
-
-			pkt[0] = V1_SIG
-			pkt[1] = V1_SOURCE_INFO
-			be.PutUint16(pkt[2:4], <-pktidq)
-			pkt[4] = 0
-			pkt[5] = 0
-
-			// source info
-
-			off = 8
-			be.PutUint32(pkt[off+0:off+4], uint32(req.data.(MreqSendCurrent).count))
-			be.PutUint64(pkt[off+4:off+12], req.data.(MreqSendCurrent).hash)
-			pkt[off+12] = V1_TYPE_STRING
-			pkt[off+13] = byte(source_len)
-			copy(pkt[off+14:], req.data.(MreqSendCurrent).source)
-			off += 14
-
-			// send the packet
-
-			for wlen = off + source_len; wlen&0x3 != 0; wlen++ {
-				pkt[wlen] = 0
-			}
-
-			be.PutUint16(pkt[6:8], uint16(wlen/4))
-
-			_, err := conn.Write(pkt[:wlen])
-			if err != nil {
-				// this will lead to re-connect and recreation of goroutines
-				connerr <- fmt.Sprintf("write error: %v", err)
-				return
-			}
-
-		case SEND_RECORDS:
-
-			if minlen := 8 + (4 + 4 + 4) + V1_AREC_LEN; len(pkt) < minlen {
-				log.Printf("E mclient write: packet buffer too short %v, needs %v",
-					len(pkt), minlen)
-				return
-			}
-
-			arecs := req.data.(MreqSendRecords).arecs
-			nrecs := len(arecs)
-
-			for ix := 0; ix < nrecs; {
-
-				// headers
-
-				pkt[0] = V1_SIG
-				pkt[1] = V1_SOURCE_RECORDS
-				be.PutUint16(pkt[2:4], <-pktidq)
-				pkt[4] = 0
-				pkt[5] = 0
-
-				be.PutUint32(pkt[8:12], uint32(req.data.(MreqSendRecords).oid))
-				be.PutUint32(pkt[12:16], uint32(req.data.(MreqSendRecords).mark))
-				pkt[16] = V1_TYPE_AREC
-				pkt[17] = V1_AREC_LEN
-
-				// records
-
-				off = 20
-				maxrecs := (len(pkt) - off) / V1_AREC_LEN
-				count := nrecs - ix
-				if count > maxrecs {
-					count = maxrecs
-				}
-
-				for maxix := ix + count; ix < maxix; ix++ {
-
-					be.PutUint32(pkt[off:off+4], uint32(arecs[ix].ea))
-					be.PutUint32(pkt[off+4:off+8], uint32(arecs[ix].ip))
-					be.PutUint32(pkt[off+8:off+12], uint32(arecs[ix].gw))
-					be.PutUint64(pkt[off+12:off+20], arecs[ix].ref.H)
-					be.PutUint64(pkt[off+20:off+28], arecs[ix].ref.L)
-					off += V1_AREC_LEN
-				}
-
-				// send the packet
-
-				wlen = off
-				be.PutUint16(pkt[18:20], uint16(count))
-				be.PutUint16(pkt[6:8], uint16(wlen/4))
-
-				_, err := conn.Write(pkt[:wlen])
-				if err != nil {
-					// this will lead to re-connect and recreation of goroutines
-					connerr <- fmt.Sprintf("write error: %v", err)
-					return
-				}
-			}
-
-		default:
-			log.Printf("E mclient write: unknown pkt type: %v", req.cmd)
+		} else {
+			log.Fatal("F dns name too long(%v): %v", dnmlen, src)
 		}
-	*/
+	}
+
+	// records
+
+	for _, rec := range req.recs {
+
+		be.PutUint32(pkt[off+V1_AREC_EA:off+V1_AREC_EA+4], uint32(0))
+		be.PutUint32(pkt[off+V1_AREC_IP:off+V1_AREC_IP+4], uint32(rec.ip))
+		be.PutUint32(pkt[off+V1_AREC_GW:off+V1_AREC_GW+4], uint32(rec.gw))
+		be.PutUint64(pkt[off+V1_AREC_REFH:off+V1_AREC_REFH+8], uint64(rec.ref.H))
+		be.PutUint64(pkt[off+V1_AREC_REFL:off+V1_AREC_REFL+8], uint64(rec.ref.L))
+		off += V1_AREC_LEN
+	}
+
+	// send the packet
+
+	be.PutUint16(pkt[V1_PKTLEN:V1_PKTLEN+2], uint16(off/4))
+
+	_, err := conn.Write(pkt[:off])
+	if err != nil {
+		// this will lead to re-connect and recreation of goroutines
+		connerr <- fmt.Sprintf("write error: %v", err)
+		return
+	}
+
 }
 
 func read_from_mapper(conn *net.UnixConn, connerr chan<- string) {
@@ -189,63 +164,59 @@ func read_from_mapper(conn *net.UnixConn, connerr chan<- string) {
 	pkt := buf[:rlen]
 
 	if pkt[0] != V1_SIG {
-		log.Printf("E mclient read: invalid pkt signature: 0x%02x", pkt[0])
+		log.Printf("E mclient read: invalid pkt signature: 0x%02x", pkt[V1_VER])
 		return
 	}
 
-	if rlen&^0x3 != 0 || uint16(rlen/4) != be.Uint16(pkt[6:8]) {
+	if rlen&^0x3 != 0 || uint16(rlen/4) != be.Uint16(pkt[V1_PKTLEN:V1_PKTLEN+2]) {
 		log.Printf("E mclient read: pkt length(%v) does not match length field(%v)",
-			rlen, be.Uint16(pkt[6:8])*4)
+			rlen, be.Uint16(pkt[V1_PKTLEN:V1_PKTLEN+2])*4)
 		return
 	}
-	/*
-		// pkt payload
 
-		cmd := pkt[1] &^ 0x3f
-		//mode := pkt[1] >> 6
-		//pktid := be.Uint16(pkt[2:4])
-		msg := pkt[8:]
+	// pkt payload
 
-		switch cmd {
-		case V1_GET_SOURCE_INFO:
+	var source string
+	var batch uint32
 
-			mreq := new(MreqData)
-			mreq.cmd = GET_CURRENT
+	off := V1_HDR_LEN
 
-			mreqq <- mreq
+payload:
+	switch pkt[V1_CMD] {
+	case V1_ACK | V1_MC_HOST_DATA:
 
-		case V1_GET_SOURCE_RECORDS:
+		batch = be.Uint32(pkt[off : off+4])
 
-			if len(msg) < 4+4+8+4 { // oid + mark + hash + minimal source
-				log.Printf("E mclient read: get record pkt too short")
-				return
+		fallthrough
+
+	case V1_DATA | V1_MC_GET_HOST_DATA:
+
+		off += 4
+
+		for off < len(pkt) {
+
+			if pkt[off] != V1_TYPE_STRING {
+				log.Printf("E mclient read: missing source string")
+				break payload
 			}
 
-			if msg[16] != V1_TYPE_STRING {
-				log.Printf("E mclient read: get record invalid string type")
-				return
-			}
-
-			if 8+4+4+8+((int(msg[17])+2+3)/4)*4 != rlen {
-				log.Printf("E mclient read: get record invalid string length(%v)", msg[17])
-				return
-			}
-
-			mreq := new(MreqData)
-			mreq.cmd = GET_RECORDS
-			mreq.data = MreqGetRecords{
-				O32(be.Uint32(msg[8:12])),
-				M32(be.Uint32(msg[12:16])),
-				be.Uint64(msg[16:24]),
-				string(msg[18 : 18+int(msg[17])]),
-			}
-
-			mreqq <- mreq
-
-		default:
-			log.Printf("E mclient read: unknown pkt type(%v)", cmd)
+			slen := int(pkt[off+1])
+			source += string(pkt[off+2:off+2+slen]) + ":"
+			off += (slen + 5) & ^3
 		}
-	*/
+
+		source = strings.TrimSuffix(source, ":")
+
+		req := HostReq{source, ACK, batch}
+		if batch == 0 {
+			req.req = RESEND
+		}
+
+		hostreqq <- req
+
+	default:
+		log.Printf("E mclient read: unknown pkt type(%v)", pkt[V1_CMD])
+	}
 }
 
 func mclient_read(conn *net.UnixConn, connerr chan<- string, quit <-chan int) {
